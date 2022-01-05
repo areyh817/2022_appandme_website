be63d3483153d2f5f1689c469041b5d0abb1cf4b
CHO HYE RA <s2037@e-mirim.hs.kr>
2022-01-05 23:58:54 +0900

    지원 데이터베이스 :dolphin:

---
+++ apply.sql
@@ -0,0 +1,8 @@
+mysql> create table apply(
+    -> id int(100) auto_increment primary key,
+    -> name varchar(20),
+    -> num int(10),
+    -> fourword varchar(12),
+    -> tel int(30),
+    -> email varchar(30)
+    -> );
