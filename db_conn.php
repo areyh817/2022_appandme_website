6039d01b8541f281a8a365350f4e73c85a8142d8
조혜라 <s2037@e-mirim.hs.kr>
2022-03-17 00:35:23 +0900

    최종파일 서버 업로드

--- db_conn.php
+++ db_conn.php
@@ -1,5 +1,5 @@
 <?php
-$conn=mysqli_connect('localhost','root','111111','appandme');
+$conn=mysqli_connect('localhost','appandme','app2022!','appandme');
 if($conn){
    // echo "mysql database에 연결되었습니다";
 }else{
--- main_member.php
+++ main_member.php
@@ -3,7 +3,7 @@
     $password=$_POST['password'];
  
 
-    if($password=="appandme") {
+    if($password=="2022") {
         header('Location:surpport_list.php');
     } else {
         echo "인증에 실패했습니다.";
