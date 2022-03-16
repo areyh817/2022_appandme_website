d947c35e66649a45430ee7e9fdcdf23c74103059
조혜라 <s2037@e-mirim.hs.kr>
2022-03-16 16:03:19 +0900

    📑 :: sql 폴더 추가

--- sql/apply.sql
+++ sql/apply.sql
@@ -2,7 +2,7 @@ mysql> create table apply(
     -> id int(100) auto_increment primary key,
     -> name varchar(20),
     -> num int(10),
-    -> fourword varchar(12),
+    -> fourword varchar(12),1
     -> tel int(30),
     -> email varchar(30)
     -> );
