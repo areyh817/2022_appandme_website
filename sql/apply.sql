7d097f91535ccd5d37a8ed1f8e1d6702522039ee
조혜라 <s2037@e-mirim.hs.kr>
2022-03-17 00:04:24 +0900

    데이터베이스 구조 변경

--- sql/apply.sql
+++
@@ -1,8 +0,0 @@
-mysql> create table apply(
-    -> id int(100) auto_increment primary key,
-    -> name varchar(20),
-    -> num int(10),
-    -> fourword varchar(12),1
-    -> tel int(30),
-    -> email varchar(30)
-    -> );
